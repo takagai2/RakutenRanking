baeafdad87b69a447fc53c36c19e3a125128fcab
takagai2 <takagai@rightcode.co.jp>
2018-03-28 13:14:19 +0900

    JsonObjectの実装

---
+++ RakutenRanking/JsonObject.swift
@@ -0,0 +1,31 @@
+//
+//  JsonObject.swift
+//  RakutenRanking
+//
+//  Created by mao takagai on 2018/03/28.
+//  Copyright © 2018年 mao takagai. All rights reserved.
+//
+
+import Foundation
+
+class JsonObject {
+    
+    // JSONをパースして特定のデータを取得する
+    
+    func getName() {
+        // TODO: 商品名を取得する処理
+    }
+    
+    func getPrice() {
+        // TODO: 値段を取得する処理
+    }
+    
+    func getImageUrl() {
+        // TODO: 画像URLを取得する処理
+    }
+    
+    func getUrl() {
+        // TODO: 商品URLを取得する処理
+    }
+    
+}
