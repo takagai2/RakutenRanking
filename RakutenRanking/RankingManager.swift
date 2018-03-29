a417cfd485c1551ce09942139134359d5b514dc8
takagai2 <takagai@rightcode.co.jp>
2018-03-29 17:20:02 +0900

    RankingManager引数修正

--- RakutenRanking/RankingManager.swift
+++ RakutenRanking/RankingManager.swift
@@ -13,75 +13,75 @@ class RankingManager {
     let rankingGateway: RankingGatewayProtocol = RankingGateway()
     
     // TODO: segmentの選択結果によって取得するランキング種別を決定
-    func getOverallRanking(url: String) {
+    func getOverallRanking() {
         rankingGateway.getOverallRankingRes()
     }
     
-    func getFemaleRanking(url: String) {
+    func getFemaleRanking() {
         rankingGateway.getFemaleRankingRes()
     }
     
-    func getMaleRanking(url: String) {
+    func getMaleRanking() {
         rankingGateway.getMaleRankingRes()
     }
     
-    func get10sRanking(url: String) {
+    func get10sRanking() {
         rankingGateway.get10sRankingRes()
     }
     
-    func get20sRanking(url: String) {
+    func get20sRanking() {
         rankingGateway.get20sRankingRes()
     }
     
-    func get30sRanking(url: String) {
+    func get30sRanking() {
         rankingGateway.get30sRankingRes()
     }
     
-    func get40sRanking(url: String) {
+    func get40sRanking() {
         rankingGateway.get40sRankingRes()
     }
     
-    func get50sOverRanking(url: String) {
+    func get50sOverRanking() {
         rankingGateway.get50sOverRankingRes()
     }
     
-    func get10sFemaleRanking(url: String) {
+    func get10sFemaleRanking() {
         rankingGateway.get10sFemaleRankingRes()
     }
     
-    func get10sMaleRanking(url: String) {
+    func get10sMaleRanking() {
         rankingGateway.get10sMaleRankingRes()
     }
     
-    func get20sFemaleRanking(url: String) {
+    func get20sFemaleRanking() {
         rankingGateway.get20sFemaleRankingRes()
     }
     
-    func get20sMaleRanking(url: String) {
+    func get20sMaleRanking() {
         rankingGateway.get20sMaleRankingRes()
     }
     
-    func get30sFemaleRanking(url: String) {
+    func get30sFemaleRanking() {
         rankingGateway.get30sFemaleRankingRes()
     }
     
-    func get30sMaleRanking(url: String) {
+    func get30sMaleRanking() {
         rankingGateway.get30sMaleRankingRes()
     }
     
-    func get40sFemaleRanking(url: String) {
+    func get40sFemaleRanking() {
         rankingGateway.get40sFemaleRankingRes()
     }
     
-    func get40sMaleRanking(url: String) {
+    func get40sMaleRanking() {
         rankingGateway.get40sMaleRankingRes()
     }
     
-    func get50sOverFemaleRanking(url: String) {
+    func get50sOverFemaleRanking() {
         rankingGateway.get50sOverFemaleRankingRes()
     }
     
-    func get50sOverMaleRanking(url: String) {
+    func get50sOverMaleRanking() {
         rankingGateway.get50sOverMaleRankingRes()
     }
     
