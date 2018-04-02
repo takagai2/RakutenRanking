eebdd4df13dfbf0ee863505ef4b69585c7b4d3dd
takagai2 <takagai@rightcode.co.jp>
2018-04-02 20:01:10 +0900

    forEach構文に書き換え

--- RakutenRanking/JsonConverter.swift
+++ RakutenRanking/JsonConverter.swift
@@ -13,18 +13,16 @@ class JsonConverter {
     func getItems(_ data: [[String]]) -> [Item] {
         var itemArray: [Item] = []
         // TODO: パース
-        
-        // TODO: Itemオブジェクト生成
-        let item = Item()
-        
-        for i in 0...29 {
-            // TODO: 該当データ抽出
-            // TODO: Itemオブジェクトにデータ代入
-            item.name = data[0][i]
-            item.price = data[1][i]
-            item.imageUrlS = data[2][i]
-            item.imageUrlM = data[3][i]
-            
+
+        data.forEach{ (datum) in
+            // Itemオブジェクトを生成
+            let item = Item()
+            // 各要素を抽出、代入
+            item.name = datum[0]
+            item.price = datum[1]
+            item.imageUrlS = datum[2]
+            item.imageUrlM = datum[3]
+            // itemArrayに追加
             itemArray.append(item)
         }
         return itemArray
