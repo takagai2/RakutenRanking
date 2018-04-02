8c25de91ac00588a9b5bb614ff2201af17b08b35
takagai2 <takagai@rightcode.co.jp>
2018-04-02 18:12:57 +0900

    getItem()の戻り値にitemArrayを設定

--- RakutenRanking/JsonConverter.swift
+++ RakutenRanking/JsonConverter.swift
@@ -10,16 +10,24 @@ import Foundation
 
 class JsonConverter {
     
-    func getItems(_ data: [[String]]) {
+    func getItems(_ data: [[String]]) -> [Item] {
+        var itemArray: [Item] = []
         // TODO: パース
-        // TODO: 該当データ抽出
+        
         // TODO: Itemオブジェクト生成
-        // TODO: Itemオブジェクトにデータ代入
-        // TODO: Arrayに追加
+        let item = Item()
+        
         for i in 0...29 {
-            let item = Item()
+            // TODO: 該当データ抽出
+            // TODO: Itemオブジェクトにデータ代入
+            item.name = data[0][i]
+            item.price = data[1][i]
+            item.imageUrlS = data[2][i]
+            item.imageUrlM = data[3][i]
             
+            itemArray.append(item)
         }
+        return itemArray
     }
     
 }
