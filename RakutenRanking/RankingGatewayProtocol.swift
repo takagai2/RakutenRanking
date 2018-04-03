0a626b5db043813808eee25495a00c9fe51bb0f4
takagai2 <takagai@rightcode.co.jp>
2018-04-03 18:22:53 +0900

    修正完了

--- RakutenRanking/RankingGatewayProtocol.swift
+++ RakutenRanking/RankingGatewayProtocol.swift
@@ -12,10 +12,10 @@ protocol RankingGatewayProtocol {
     
     func getOverallRankingRes(_ callback: ([Item]) -> Void)
     
-    func getRankingByGenderRes(gender: Gender, _ closure: ([Item]) -> Void)
+    func getRankingByGenderRes(gender: Gender, _ callback: ([Item]) -> Void)
     
-    func getRankingByAgeRes(age: Age, _ closure: ([Item]) -> Void)
+    func getRankingByAgeRes(age: Age, _ callback: ([Item]) -> Void)
     
-    func getRankingByGenderAgeRes(gender: Gender, age: Age, _ closure: ([Item]) -> Void)
+    func getRankingByGenderAgeRes(gender: Gender, age: Age, _ callback: ([Item]) -> Void)
     
 }
