32b2e075b98e5a8f3aebc441fb00fbf3d057561e
takagai2 <takagai@rightcode.co.jp>
2018-04-04 18:47:06 +0900

    callbakで返したitemArrayをRankingManagerで保持

--- RakutenRanking/RankingManager.swift
+++ RakutenRanking/RankingManager.swift
@@ -26,12 +26,14 @@ enum Age {
 class RankingManager {
     
     private let rankingGateway: RankingGatewayProtocol = RankingGateway()
+    var itemArray: [Item] = []
 
     // 総合ランキング
     func getOverallRanking() {
         rankingGateway.getOverallRankingRes({(array: [Item]) -> Void in
             // TODO: Realmに保存する処理
             // TODO: XXXViewControllerにItemを渡す処理
+            itemArray = array
         })
         
     }
