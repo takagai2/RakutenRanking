d33eee82e11ade3242f878f7e19a57b0c96788f9
takagai2 <takagai@rightcode.co.jp>
2018-04-12 13:06:17 +0900

    不要なSwitch文の削除

--- RakutenRanking/RankingGateway.swift
+++ RakutenRanking/RankingGateway.swift
@@ -55,7 +55,7 @@ class RankingGateway: RankingGatewayProtocol {
     }
     
     func getOverallRankingRes(_ callback: ([Item]) -> Void) {
-        // TODO: 通信処理を行って対象のランキングのレスポンスを取得
+        // 通信処理を行って対象のランキングのレスポンスを取得
         self.createUrl(gender: nil, age: nil)
         
         // seccess -> パース -> 空配列へ
@@ -64,15 +64,8 @@ class RankingGateway: RankingGatewayProtocol {
     }
     
     func getRankingByGenderRes(gender: Gender, _ callback: ([Item]) -> Void) {
-        // TODO: 通信処理を行って対象の男女別ランキングのレスポンスを取得
-        switch gender {
-        case .female:
-            // 女性総合ランキング取得
-            self.createUrl(gender: gender, age: nil)
-        case .male:
-            // 男性総合ランキング取得
-            self.createUrl(gender: gender, age: nil)
-        }
+        // 通信処理を行って対象の男女別ランキングのレスポンスを取得
+        self.createUrl(gender: gender, age: nil)
         
         // seccess -> パース -> 空配列へ
         let itemArray = jsonConverter.getItems(testArray)
@@ -80,24 +73,8 @@ class RankingGateway: RankingGatewayProtocol {
     }
     
     func getRankingByAgeRes(age: Age, _ callback: ([Item]) -> Void) {
-        // TODO: 通信処理を行って対象の年齢別ランキングのレスポンスを取得
-        switch age {
-        case .teens:
-            // 10代総合ランキング取得
-            self.createUrl(gender: nil, age: age)
-        case .twenties:
-            // 20台総合ランキング取得
-            self.createUrl(gender: nil, age: age)
-        case .thirties:
-            // 30台総合ランキング取得
-            self.createUrl(gender: nil, age: age)
-        case .forties:
-            // 40代総合ランキング取得
-            self.createUrl(gender: nil, age: age)
-        case .fiftiesOver:
-            // 50代以上総合ランキング取得
-            self.createUrl(gender: nil, age: age)
-        }
+        // 通信処理を行って対象の年齢別ランキングのレスポンスを取得
+        self.createUrl(gender: gender, age: nil)
         
         // seccess -> パース -> 空配列へ
         let itemArray = jsonConverter.getItems(testArray)
@@ -105,50 +82,8 @@ class RankingGateway: RankingGatewayProtocol {
     }
     
     func getRankingByGenderAgeRes(gender: Gender, age: Age, _ callback: ([Item]) -> Void) {
-        // TODO: 通信処理を行って対象のランキングのレスポンスを取得
-        switch age {
-        case .teens:
-            if gender == .female {
-                // 10代女性総合ランキング取得
-                self.createUrl(gender: gender, age: age)
-                
-            } else {
-                // 10代男性総合ランキング取得
-                self.createUrl(gender: gender, age: age)
-            }
-        case .twenties:
-            if gender == .female {
-                // 20代女性総合ランキング取得
-                self.createUrl(gender: gender, age: age)
-            } else {
-                // 20代男性総合ランキング取得
-                self.createUrl(gender: gender, age: age)
-            }
-        case .thirties:
-            if gender == .female {
-                // 30代女性総合ランキング取得
-                self.createUrl(gender: gender, age: age)
-            } else {
-                // 30代男性総合ランキング取得
-                self.createUrl(gender: gender, age: age)
-            }
-        case .forties:
-            if gender == .female {
-                // 40代女性総合ランキング取得
-                self.createUrl(gender: gender, age: age)
-            } else {
-                // 40代男性総合ランキング取得
-                self.createUrl(gender: gender, age: age)
-            }
-        case .fiftiesOver:
-            if gender == .female {
-                // 50代以上女性総合ランキング取得
-                self.createUrl(gender: gender, age: age)
-            } else {
-                // 50代以上男性総合ランキング取得
-                self.createUrl(gender: gender, age: age)
-            }
-        }
+        // 通信処理を行って対象のランキングのレスポンスを取得
+        self.createUrl(gender: gender, age: nil)
         
         // seccess -> パース -> 空配列へ
         let itemArray = jsonConverter.getItems(testArray)
