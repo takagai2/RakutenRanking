c45fcdc59fcde52e18fa8dfaf56101390e49b7fe
takagai2 <takagai@rightcode.co.jp>
2018-04-12 19:00:56 +0900

    jsonをパースしてitemArrayを作成

--- RakutenRanking/JsonConverter.swift
+++ RakutenRanking/JsonConverter.swift
@@ -7,26 +7,39 @@
 //
 
 import Foundation
+import SwiftyJSON
 
 class JsonConverter {
     
     func getItems(_ data: Any?) -> [Item] {
-        print(data)
         var itemArray: [Item] = []
         // TODO: パース
-
-//        data.forEach{ (datum) in
-//            // Itemオブジェクトを生成
-//            let item = Item()
-//            // 各要素を抽出、代入
-//            item.name = datum[0]
-//            item.price = datum[1]
-//            item.sSizeImageUrl = datum[2]
-//            item.mSizeImageUrl = datum[3]
-//            // itemArrayに追加
-//            itemArray.append(item)
-//        }
+        let json = JSON(data!)
+        
+        for i in 0...30 {
+            // Itemオブジェクトを生成
+            let item = Item()
+            // 各要素を抽出、代入
+            if let itemName = json["Items"][i]["Item"]["itemName"].string {
+                item.name = itemName
+                print(itemName)
+            }
+            if let itemPrice = json["Items"][i]["Item"]["itemPrice"].string {
+                item.price = itemPrice
+                print(itemPrice)
+            }
+            if let smallImageUrl = json["Items"][i]["Item"]["smallImageUrls"][0]["imageUrl"].string {
+                item.sSizeImageUrl = smallImageUrl
+                print(smallImageUrl)
+            }
+            if let mediumImageUrl = json["Items"][i]["Item"]["mediumImageUrls"][0]["imageUrl"].string {
+                item.mSizeImageUrl = mediumImageUrl
+                print(mediumImageUrl)
+            }
+            // itemArrayに追加
+            itemArray.append(item)
+        }
+        print(itemArray)
         return itemArray
-    }
-    
+        }
 }
