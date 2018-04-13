e6a5e2e0705d5ca0616fd6cc9a14e264216dc5e3
takagai2 <takagai@rightcode.co.jp>
2018-04-13 14:09:38 +0900

    jsonのデータを格納する変数名及びforEachの引数名を変更

--- RakutenRanking/JsonConverter.swift
+++ RakutenRanking/JsonConverter.swift
@@ -14,16 +14,16 @@ class JsonConverter {
     func getItems(_ data: Any?, _ callback: ([Item]) -> Void) {
         var itemArray: [Item] = []
         // パース
-        let json = JSON(data!)
+        let rankingJson = JSON(data!)
         
-        json["Items"].forEach { (_, json) in
+        rankingJson["Items"].forEach { (_, object) in
             // Itemオブジェクトを生成
             let item = Item()
             // 各要素を抽出、代入
-            item.name = json["Item"]["itemName"].string!
-            item.price = json["Item"]["itemPrice"].string!
-            item.sSizeImageUrl = json["Item"]["smallImageUrls"][0]["imageUrl"].string!
-            item.mSizeImageUrl = json["Item"]["mediumImageUrls"][0]["imageUrl"].string!
+            item.name = object["Item"]["itemName"].string!
+            item.price = object["Item"]["itemPrice"].string!
+            item.sSizeImageUrl = object["Item"]["smallImageUrls"][0]["imageUrl"].string!
+            item.mSizeImageUrl = object["Item"]["mediumImageUrls"][0]["imageUrl"].string!
             // itemArrayに追加
             itemArray.append(item)
         }
