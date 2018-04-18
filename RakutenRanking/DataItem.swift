876bf95d5af3d69d6e220a1b70548989fa647823
takagai2 <takagai@rightcode.co.jp>
2018-04-18 10:38:41 +0900

    保存するデータを設定

---
+++ RakutenRanking/DataItem.swift
@@ -0,0 +1,18 @@
+//
+//  DataItem.swift
+//  RakutenRanking
+//
+//  Created by mao takagai on 2018/04/18.
+//  Copyright © 2018年 mao takagai. All rights reserved.
+//
+
+import Foundation
+
+class DataItem {
+    
+    @objc dynamic var name: String!
+    @objc dynamic var price: String!
+    @objc dynamic var sSizeImageUrl: String!
+    @objc dynamic var mSIzeImageUrl: String!
+    
+}
---
+++ RakutenRanking/DataObject.swift
@@ -0,0 +1,32 @@
+//
+//  DataObject.swift
+//  RakutenRanking
+//
+//  Created by mao takagai on 2018/04/18.
+//  Copyright © 2018年 mao takagai. All rights reserved.
+//
+
+import Foundation
+
+class DataObject {
+    
+    var overallRanking = List<DataItem>()
+    var maleRanking = List<DataItem>()
+    var femaleRanking = List<DataItem>()
+    var teensRanking = List<DataItem>()
+    var twentiesRanking = List<DataItem>()
+    var thirtiesRanking = List<DataItem>()
+    var fortiesRanking = List<DataItem>()
+    var fiftiesOverRanking = List<DataItem>()
+    var maleTeensRanking = List<DataItem>()
+    var maleTwentiesRanking = List<DataItem>()
+    var maleThirtiesRanking = List<DataItem>()
+    var maleFortiesRanking = List<DataItem>()
+    var maleFiftiesRanking = List<DataItem>()
+    var femaleTeensRanking = List<DataItem>()
+    var femaleTwentiesRanking = List<DataItem>()
+    var femaleThirtiesRanking = List<DataItem>()
+    var femaleFortiesRanking = List<DataItem>()
+    var femaleFiftiesRanking = List<DataItem>()
+    
+}
