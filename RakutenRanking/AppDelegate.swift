c73d8416e25839dfca3dacace297681978a063b2
takagai2 <takagai@rightcode.co.jp>
2018-04-18 18:33:07 +0900

    アプリ終了時にRealmを初期化

--- RakutenRanking/AppDelegate.swift
+++ RakutenRanking/AppDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import RealmSwift
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -39,6 +40,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func applicationWillTerminate(_ application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+        let realm = try! Realm()
+        try! realm.write() {
+            // TODO: お気に入り機能実装時はお気に入り指定したアイテムを残す
+            realm.deleteAll()
+            print("データ削除")
+        }
     }
 
 
