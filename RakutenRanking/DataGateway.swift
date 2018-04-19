f569b71ebb66d890092923bb3a2aaef91b5034c1
takagai2 <takagai@rightcode.co.jp>
2018-04-19 14:48:44 +0900

    DataGateway作成

---
+++ RakutenRanking/DataGateway.swift
@@ -0,0 +1,16 @@
+//
+//  DataGateway.swift
+//  RakutenRanking
+//
+//  Created by mao takagai on 2018/04/19.
+//  Copyright © 2018年 mao takagai. All rights reserved.
+//
+
+import Foundation
+import RealmSwift
+
+class DataGateway {
+    
+    let realm = try! Realm()
+    
+}
