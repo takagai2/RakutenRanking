c20fed1c817263ca5b9e543440f10721eff2d0cd
takagai2 <takagai@rightcode.co.jp>
2018-04-24 13:36:08 +0900

    FavoriteObjectにDataObjectを継承させる

--- RakutenRanking/FavoriteObject.swift
+++ RakutenRanking/FavoriteObject.swift
@@ -9,22 +9,8 @@
 import Foundation
 import RealmSwift
 
-class FavoriteObject: Object {
+class FavoriteObject: DataObject {
     
-    @objc dynamic var id = 0
-    @objc dynamic var name = ""
-    @objc dynamic var price = ""
-    @objc dynamic var sSizeImageUrl = ""
-    @objc dynamic var mSizeImageUrl = ""
-    @objc dynamic var itemCode = ""
     @objc dynamic var date = ""
     
-    override static func primaryKey() -> String? {
-        return "id"
-    }
-    // 新しいidを取得
-    func createNewId() -> Int {
-        let realm = try! Realm()
-        return (realm.objects(type(of: self).self).sorted(byKeyPath: "id", ascending: false).first?.id ?? 0) + 1
-    }
 }
