1e6e472574a34e9612dd5e45b68994e6bcc1f7b5
takagai2 <takagai@rightcode.co.jp>
2018-05-09 15:24:44 +0900

    SlideMenuControllerSwiftのインストール

---
+++ RakutenRanking/SlideViewController.swift
@@ -0,0 +1,9 @@
+//
+//  SlideViewController.swift
+//  RakutenRanking
+//
+//  Created by mao takagai on 2018/05/09.
+//  Copyright © 2018年 mao takagai. All rights reserved.
+//
+
+import Foundation
